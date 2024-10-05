20c78d7a3961ba5b27fa34b11746f8af0684442c
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2024-10-05 14:04:32 -0400

    Update Madlib.py

--- Madlib.py
+++ Madlib.py
@@ -7,4 +7,4 @@ color3 = input ("Choose one more color: ")
 adjective1 = input ("Choose an adjective: ")
 adjective2 = input ("Choose another adjective: ")
 
-print ("One " + adjectove1 + color1 + animal + "went down to the " + color2 + "store and met up with a " + adjective2 + color3 + animal2
+print ("One " + adjective1 + color1 + animal + "went down to the " + color2 + "store and met up with a " + adjective2 + color3 + animal2)
