9cd1e59beb2cfb7754cd456b94b5c6fc9f7ede04
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2024-10-05 14:23:36 -0400

    Update Knock Knock Joke.py

--- Knock Knock Joke.py
+++ Knock Knock Joke.py
@@ -3,20 +3,20 @@
 name = input("Enter your name: ")
 
 # Ask if they want to hear a knock knock joke -> question
-question = input("Hello " + name + "! Would you like to hear a knock knock joke?")
+question = input(f"Hello " {name} "! Would you like to hear a knock knock joke?")
 
 if question == "yes":
     print("Alright alright alright!")
+else:
+    print("awww, ok. Have a good day then.")
     
     response = input ("Knock Knock... ")
     if response.upper() == "WHO'S THERE?":
         response2 = input("Never gonna… ")
-        if response2.upper() == "NEVER GONNA WHAT?":
-             print("Never Gonna Give You Up, Never Gonna Let You Down, Never Gonna Run Around And Hurt You!")
-         else:
-             print("If you didn’t want to hear the joke, you could’ve just said no earlier…")
     else:
         print("You were supposed to say who's there")
-
-else:
-    print("awww, ok. Have a good day then.")
+        
+    if response2.upper() == "NEVER GONNA WHAT?":
+             print("Never Gonna Give You Up, Never Gonna Let You Down, Never Gonna Run Around And Hurt You!")
+    else:
+             print("If you didn’t want to hear the joke, you could’ve just said no earlier…")
